d3e47730dd4f8af341167aba5137aeea052f6c41
WilliamLewww <WilliamLew32@gmail.com>
2015-11-27 01:23:59 -0800

    Font-Separation

---
+++ FontSeparation/FontSeparation.cs
@@ -0,0 +1,37 @@
+﻿using System.Drawing;
+using System.Collections.Generic;
+using System;
+
+namespace FontSeparation
+{
+    class FontSeparation
+    {
+        static Bitmap font;
+        public static List<int[]> coordinateList = new List<int[]>();
+
+        public void LoadBitmap(Bitmap bitmap)
+        {
+            font = bitmap;
+
+            bool a = false, b = false;
+
+            for (int x = 0; x < bitmap.Width; x++)
+            {
+                b = false;
+
+                for (int y = 0; y < bitmap.Height; y++)
+                {
+                    if (bitmap.GetPixel(x, y).A != 0)
+                    {
+                        b = true;
+                    }
+
+                    if (y == bitmap.Height - 1 && b == true)
+                        a = true;
+                    else if (y == bitmap.Height - 1 && b == false)
+                        a = false;
+                }
+            }
+        }
+    }
+}
---
+++ FontSeparation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FontSeparation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FontSeparation fontSeparation = new FontSeparation();
+            fontSeparation.LoadBitmap(new System.Drawing.Bitmap("font.png"));
+
+            Console.ReadKey();
+        }
+    }
+}
